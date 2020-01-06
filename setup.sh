b4d8b8c19a97719961a6daafd531089e65e73edf
Stephen Tierney <sjtrny@gmail.com>
2020-01-06 19:42:31 +1100

    Fixed bugs

--- setup.sh
+++ setup.sh
@@ -1,13 +1,15 @@
 # Update apt
-apt-get update
+apt-get -y update
+
+apt-get -y install -y i2c-tools
 
 # Create virtual env and install requirements
-apt-get install python3-venv
+apt-get -y install python3-venv
 python3 -m venv venv
 source venv/bin/activate
-python3 -m pip install -r requirements.txt
+venv/bin/python3 -m pip install -r requirements.txt
 
-# Enable and run poseidon service
+# Enable and run gaia service
 cp gaia.service /etc/systemd/system
 systemctl enable gaia
 systemctl start gaia
